ae3104252617e4e1a0b07b05431befe9bd1fe71a
Sadia Batool <sadia110@my.yorku.ca>
2020-07-24 17:05:47 -0400

    fixed conn with sql

--- schema.sql
+++ schema.sql
@@ -6,10 +6,10 @@ flush privileges;
 # Creating a database 
 CREATE DATABASE employedata; 
 
-# Switching to the database 
+#Switching to the database 
 USE employedata; 
 
-# Create Table
+#Create Table. 
 CREATE TABLE department (
 	id INT AUTO_INCREMENT PRIMARY KEY, 
 	name VARCHAR (30)
@@ -32,13 +32,26 @@ CREATE TABLE employee (
 
 
 # Inserting data into table  
-INSERT INTO department VALUE (1, "Web Design" );
+INSERT INTO department VALUE (1, "Web Dev" );
+INSERT INTO department VALUE (1, "HR" ); 
+INSERT INTO department VALUE (1, "Sales" ); 
+INSERT INTO department VALUE (1, "Admin" );
 
-INSERT INTO role VALUES(1,"Developer", "50000.00"); 
+
+
+
+
+
+INSERT INTO role VALUES(1,"Senior Developer", "50000.00");  
+INSERT INTO role VALUES(1,"Junior Developer", "30000.00");   
+INSERT INTO role VALUES(1,"FrontEnd Developer", "40000.00"); 
+INSERT INTO role VALUES(1,"BackEnd Developer", "40000.00");    
+
 
 INSERT INTO employee VALUES(1,"Employee1", "Lastname1", 1, 1 ); 
 INSERT INTO employee VALUES(0,"Employee2", "Lastname2", 1, 1 ); 
-INSERT INTO employee VALUES(0,"Employee3", "Lastname3", 1, 1 ); 
+INSERT INTO employee VALUES(0,"Employee3", "Lastname3", 1, 1 );  
 
-SELECT * FROM employee;
+
+SELECT * FROM employee 
 
