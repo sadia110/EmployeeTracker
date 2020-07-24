4150d20f04bf567f87fbd08b938127349ec08180
Sadia Batool <sadia110@my.yorku.ca>
2020-07-23 22:19:09 -0400

    setting up qustions and entries

--- server.js
+++ server.js
@@ -28,14 +28,7 @@ class Database {
   }
 }
 
-// const db = new Database ({
-//     host: "localhost", 
-//     port: 3306, 
-//     user: 'root',
-//     password: 'Batool12',
-//     database: 'employedata',
-//     insecureAuth : true
-// });    
+  
 
 const db = new Database ({
     host: "localhost", 
@@ -47,14 +40,120 @@ const db = new Database ({
 }); 
 
 
+// async function mainApp(){ 
+
+//     let employeeList = await db.query( "SELECT * FROM employee" ) 
+//     console.log ( employeeList ) 
+// } 
+
+let response 
+let role, department
+
 async function mainApp(){
-    let employeeList = await db.query( "SELECT * FROM employee" ) 
-    console.log ( employeeList ) 
-} 
+    response = await inquirer.prompt([
+        {   
+            message: "What do you want to do?", 
+            type: "list", 
+            name: "action", 
+            choices: [ 
+                {
+                     name: "Manage Departments", 
+                     value: "department" 
+                    },
+                { 
+                    name: "Manage Roles", 
+                    value: "role" 
+                }, 
+                { 
+                    name: "Manage Employees", 
+                    value: "employee"  
+                } 
+            ] 
+        }
+    ])
 
-// console.log( db )
+    console.log( response.action )  
+
+   
+        response = await inquirer.prompt([ 
+            {   
+                message: "What would you like to do next?",
+                type: "list", 
+                name: "action", 
+                choices: [
+                    { 
+                        name: "Update Employee Role", 
+                        value: "update" 
+                    }, 
+                    { 
+                        name: "Add Employee", 
+                        value: "add" 
+                    },
+                    { 
+                        name: "Return to the  menu", 
+                        value: "return" 
+                    } 
+                ] 
+            }
+        ])   
+    
+        
+        console.log( response.action ) 
+    
 
 
-mainApp();
+        if( response.action=="add" ){
+            const dbRole = await db.query( "SELECT * FROM role")
+            role = []
+            dbRole.forEach( function( item ){
+                role.push( { name: item.title, value: item.id } )
+            })
+            console.log( `role: `, role ) 
+
+            response = await inquirer.prompt([
+                {  
+                     message: "What is their first name?", 
+                    type: "input", 
+                    name: "first_name" 
+                },
+                {   
+                    message: "What is their last name?",    
+                    type: "input", 
+                    name: "last_name" 
+                },
+                {   
+                    message: "What is their role", 
+                    type: "list", 
+                    name: "role",
+                    choices: role  
+                },
+                {   
+                    message: "Who is their manager?",   
+                    type: "list", 
+                    name: "department",
+                    choices: [ "no one"] 
+                }                    
+            ]) 
+
+            console.log(  `user info: `, response )
+
+
+            // to save into DB
+            let saveResult = await db.query( "INSERT INTO employee VALUES( ?,?,?,?,? ) ", 
+                                            [ 0, response.first_name, response.last_name, response.role, 1 ] )
+            console.log( `.. user saved: new user-id=`, saveResult.insertId )  
+
+        } 
+        if( response.action=="return" ){
+            mainApp()
+        }
+    }
+
+
+
+
+
+
+
 
 
