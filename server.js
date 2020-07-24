6f818de5f9d8b57ba06a8b786de3221c1293fdbf
Sadia Batool <sadia110@my.yorku.ca>
2020-07-24 17:10:21 -0400

    add employee

--- server.js
+++ server.js
@@ -40,8 +40,123 @@ const db = new Database ({
 }); 
 
 
+
+
+let response
+let role, department
+
+async function mainApp(){
+    response = await inquirer.prompt([
+        {       
+            message: "What do you want to do?",
+            type: "list",      
+            name: "action", 
+            choices: [ 
+                {   
+                    name: "Manage Departments",     
+                    value: "department"     
+                },
+                {   
+                    name: "Manage Roles",   
+                    value: "role"   
+                }, 
+                {   
+                    name: "Manage Employees", 
+                    value: "employee" 
+                } ] }
+    ])
+
 async function mainApp(){ 
 
     let employeeList = await db.query( "SELECT * FROM employee" ) 
     console.log ( employeeList ) 
 } 
+   //  table to join employee name 
+   if( response.action=="employee" ){
+    let employeeList = await db.query( 
+        "SELECT e.id," +
+        "CONCAT(e.first_name,' ',e.last_name) AS employeeName,"+
+        "CONCAT(m.first_name,' ',m.last_name) AS managerName,r.title,r.salary "+
+        "FROM employee AS e "+
+        "LEFT JOIN employee AS m ON(e.manager_id=m.id) "+
+        "LEFT JOIN role AS r ON(e.role_id=r.id)" )
+
+    console.table( employeeList ) 
+
+
+    response = await inquirer.prompt([
+        {       
+            message: "What do you want to do now?", 
+            type: "list", 
+            name: "action", 
+            choices: [
+                {   
+                    name: "Update Employee Role", 
+                    value: "update" 
+                }, 
+                {   
+                    name: "Add Employee", 
+                    value: "add" 
+                },
+                {   
+                    name: "Return to the main menu", 
+                    value: "return" 
+                } 
+            ] 
+        }
+    ])        
+
+
+
+
+
+// ADD EMPLOYEES
+    if( response.action=="add" ){
+        const dbRole = await db.query( "SELECT * FROM role")
+        role = []
+        dbRole.forEach( function( item ){
+            role.push( { name: item.title, value: item.id } )
+        })
+                    
+        response = await inquirer.prompt([
+            {       
+                message: "Enter employee  first name", 
+                type: "input", 
+                name: "first_name" 
+            },
+            {       
+                message: "Enter employee last name", 
+                type: "input", 
+                name: "last_name" 
+            },
+            {   
+                 message: "Enter employee role", 
+                 type: "list", 
+                 name: "role",
+                choices: role 
+            },
+            {   
+                 message: "Enter employee  manager", 
+                 type: "list", 
+                 name: "department",
+                choices: [ "no one"] 
+            }                    
+        ]) 
+
+        //INSERT INTO employee VALUES( 0, "Employee2", "Lastname2", 1, 1 );
+        let saveResult = await db.query( "INSERT INTO employee VALUES( ?,?,?,?,? ) ", 
+                                        [ 0, response.first_name, response.last_name, response.role, 1 ] )
+        console.log( `Employee ${response.first_name + response.last_name} has been added to the database.` )
+        mainApp()
+
+    }
+
+
+    if( response.action=="return" ){
+        console.log( `Returning to the main menu...`)
+        mainApp()
+    }
+}
+
+}
+mainApp()
